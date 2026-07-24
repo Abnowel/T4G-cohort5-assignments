ca23683c4c460668001c98eb4cfeee14c3e5d5ee
Abnowel <abimaame77@gmail.com>
2026-07-24 23:09:31 +0000

    calculate interest,display savings account details

--- savings_account.py
+++ savings_account.py
@@ -5,3 +5,12 @@ class SavingsAccount(BankAccount):
     def _init_(self, account_holder, balance, interest_rate):
         super()._init_(account_holder, balance)
         self.interest_rate = interest_rate
+
+    # Calculates interest based on the current balance and adds the interest to the account.
+    def apply_interest(self):
+        interest = self.starting_balance * (self.interest_rate / 100)
+        self.deposit(interest)
+
+    # Displays the savings account details and interest rate.
+    def _str_(self):
+        return f"SavingsAccount[{self.account_holder}] | Balance: GHS {self.starting_balance:.2f} | Rate: {self.interest_rate}%"
