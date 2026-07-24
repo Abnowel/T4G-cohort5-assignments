6b21429a60c17ecdaf00be8767ab4e2ea1e4706b
Abnowel <abimaame77@gmail.com>
2026-07-24 22:07:06 +0000

    adding methods for managing a bank account instance

--- bank_account.py
+++ bank_account.py
@@ -1,8 +1,32 @@
-"""creating a class BankAccount"""
 class BankAccount:
-    def __init__(self,account_holder,starting_balance):
+    """creating a class BankAccount"""
+    def __init__(self, account_holder, balance):
         self.account_holder = account_holder
-        self.starting_balance  = starting_balance
+        self.balance  = balance
+
+    """Methods for managing a BankAccount instance"""
+    def deposit(self,amount):
+        if amount <= 0:
+            raise ValueError("Deposit must be greater than zero")
+        self.balance += amount
+
+    def withdraw(self,amount):
+        if amount<= 0:
+            raise ValueError("Withdrawal amount must be greater than zero.")
+        if amount > self.balance:
+            raise ValueError("Insufficient funds,Kindly load your account.")
+        self.balance -= amount
+
+    def get_balance(self):
+        return self.balance
+    """Display when account is printed"""
+    def __str__(self):
+        return f"Account[{self.account_holder}]| Balance: GHS {self.balance:.2f}"
 
     
+
+        
+
+        
+        
         
