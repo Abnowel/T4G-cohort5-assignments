217bf7fc90eff148433ed97e9387df21b303d91d
Abnowel <abimaame77@gmail.com>
2026-07-24 20:58:35 +0000

    creating a class bankaccount

---
+++ bank_account.py
@@ -0,0 +1,8 @@
+"""creating a class BankAccount"""
+class BankAccount:
+    def __init__(self,account_holder,starting_balance):
+        self.account_holder = account_holder
+        self.starting_balance  = starting_balance
+
+    
+        
